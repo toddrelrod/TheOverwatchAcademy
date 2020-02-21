7b9d78819c46bc3244d7eeadd0239b27aae1b92b
Jennifer White <ninjettey@gmail.com>
2020-02-20 20:22:00 -0800

    my branch

--- js/script.js
+++ js/script.js
@@ -17,13 +17,12 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
-
-
 // Get the modal
 var modal = document.getElementById('id01');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";}
+        modal.style.display = "none";
+    }
 }
