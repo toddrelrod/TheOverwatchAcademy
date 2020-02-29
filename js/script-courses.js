27172384c96995cfedc46743a15340a470ef8612
Todd Elrod <tre@Todds-MacBook-Pro.local>
2020-02-28 18:24:37 -0800

    updated calendar and course work

--- js/script-courses.js
+++ js/script-courses.js
@@ -17,3 +17,18 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
